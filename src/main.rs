e9e0519df967b3273a6d3a7ef338b12c3618dafc
Kush Jain <kush.jain@innovaccer.com>
2025-03-05 01:03:15 +0530

    Return HTTP Response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[allow(unused_imports)]
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -9,8 +10,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
