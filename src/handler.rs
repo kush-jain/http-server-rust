4afe95e9db5023ed373d0a2ea14a4ced47685489
Kush Jain <kush.jain@innovaccer.com>
2025-03-05 23:20:58 +0530

    Added user-agent route

--- src/handler.rs
+++ src/handler.rs
@@ -14,6 +14,18 @@ fn handle_echo(content: &String) -> Vec<u8> {
     response.into_bytes()
 }
 
+fn handle_user_agent(headers: &Vec<String>) -> Vec<u8> {
+    let user_agent = headers.iter().find(|header| header.starts_with("User-Agent: "));
+    let response = match user_agent {
+        Some(ua) => {
+            let ua_value = ua.replace("User-Agent: ", "");
+            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", ua_value.len(), ua_value)
+        },
+        None => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+    };
+    response.into_bytes()
+}
+
 
 pub fn handle_http_request(request: &[String]) -> Result<Vec<u8>, Box<dyn Error>> {
 
@@ -23,6 +35,7 @@ pub fn handle_http_request(request: &[String]) -> Result<Vec<u8>, Box<dyn Error>
     }
 
     let request_line = &request[0];
+    let headers = &request[1..];
     println!("received request: {:?}", request_line);
 
     let request_components: Vec<&str> = request_line.split_whitespace().collect();
@@ -37,6 +50,7 @@ pub fn handle_http_request(request: &[String]) -> Result<Vec<u8>, Box<dyn Error>
     let response = match route {
         "/" => handle_root(),
         r if r.starts_with("/echo/") => handle_echo(&r[6..].to_string()),
+        "/user-agent" => handle_user_agent(&headers.to_vec()),
         _ => handle_default(),
     };
 
@@ -67,6 +81,13 @@ mod tests {
         let request = vec!["GET /echo/foo HTTP/1.1".to_string()];
         let response = handle_http_request(&request).unwrap();
         assert_eq!(response, b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nfoo");
+    }
+
+    #[test]
+    fn handle_http_request_user_agent_route() {
+        let request = vec!["GET /user-agent HTTP/1.1".to_string(), "User-Agent: curl/7.64.1".to_string()];
+        let response = handle_http_request(&request).unwrap();
+        assert_eq!(response, b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\ncurl/7.64.1");
     }
 
     #[test]
