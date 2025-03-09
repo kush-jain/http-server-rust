4dc95128a8639545b7c677c2493ad7137917bdda
Kush Jain <kush.jain@innovaccer.com>
2025-03-09 22:02:36 +0530

    Added Post file route, refactored http_handler to get inputs

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 
 fn handle_root() -> Vec<u8> {
@@ -29,13 +29,10 @@ fn handle_user_agent(headers: &Vec<String>) -> Vec<u8> {
     response.into_bytes()
 }
 
-fn handle_file_route(file_path: &String) -> Vec<u8> {
 
-    let directory = std::env::var("APP_DIRECTORY").unwrap_or_else(|_| ".".to_string());
+fn handle_read_file(file_path: &PathBuf) -> String {
 
-    let path = Path::new(&directory).join(file_path);
-    println!("file path: {:?}", path);
-    let content = fs::read_to_string(path);
+    let content = fs::read_to_string(file_path);
 
     let response = match content {
         Ok(text) => {
@@ -44,20 +41,44 @@ fn handle_file_route(file_path: &String) -> Vec<u8> {
         },
         Err(_e) => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
     };
+    response
+}
+
+
+fn handle_write_file(file_path: &PathBuf, content: &String) -> String {
+
+    println!("Writing to file: {:?} contents: {:?}", file_path, content);
+    let write_response = fs::write(file_path, content);
+
+    let response = match write_response {
+        Ok(_) => "HTTP/1.1 201 Created\r\n\r\n".to_string(),
+        Err(_e) => "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string()
+    };
+    response
+}
+
+
+fn handle_file_route(file_path: &String, method: &str, request_body: &String) -> Vec<u8> {
+
+    let directory = std::env::var("APP_DIRECTORY").unwrap_or_else(|_| ".".to_string());
+
+    let path = Path::new(&directory).join(file_path);
+    println!("file path: {:?}", path);
+
+    let response = match method.to_uppercase().as_str() {
+        "GET" => handle_read_file(&path),
+        "POST" => handle_write_file(&path, request_body),
+        _ => "HTTP/1.1 405 Method Not Allowed\r\n\r\n".to_string()
+    };
     response.into_bytes()
 }
 
 
-pub fn handle_http_request(request: &[String]) -> Result<Vec<u8>, Box<dyn Error>> {
+pub fn handle_http_request(
+    request_line: &String, headers: &Vec<String>, request_body: &String
+) -> Result<Vec<u8>, Box<dyn Error>> {
 
-    if request.is_empty() {
-        println!("Empty request");
-        return Err("Empty request".into());
-    }
-
-    let request_line = &request[0];
-    let headers = &request[1..];
-    println!("received request: {:?}", request_line);
+    println!("received request: {:?}, headers: {:?}, request body: {:?}", request_line, headers, request_body);
 
     let request_components: Vec<&str> = request_line.split_whitespace().collect();
 
@@ -66,13 +87,16 @@ pub fn handle_http_request(request: &[String]) -> Result<Vec<u8>, Box<dyn Error>
         return Err("Invalid request line".into());
     }
 
+    let method = request_components[0];
     let route = request_components[1];
+
+    println!("method: {:?}, route: {:?}", method, route);
 
     let response = match route {
         "/" => handle_root(),
         r if r.starts_with("/echo/") => handle_echo(&r[6..].to_string()),
-        r if r.starts_with("/files/") => handle_file_route(&r[7..].to_string()),
-        "/user-agent" => handle_user_agent(&headers.to_vec()),
+        r if r.starts_with("/files/") => handle_file_route(&r[7..].to_string(), method, &request_body),
+        "/user-agent" => handle_user_agent(&headers),
         _ => handle_default(),
     };
 
@@ -82,43 +106,53 @@ pub fn handle_http_request(request: &[String]) -> Result<Vec<u8>, Box<dyn Error>
 
 #[cfg(test)]
 mod tests {
-    use std::env;
+    use std::{env, vec};
     use super::*;
     use crate::utils;
 
+    fn get_inputs(method: &str, route: &str, headers: Option<Vec<&str>>, body: Option<&str>) -> (String, Vec<String>, String) {
+        let request_line = format!("{} {} HTTP/1.1", method, route);
+        let headers = headers.unwrap_or_else(|| vec![]);
+        let body = body.unwrap_or_else(|| "");
+        (request_line, headers.iter().map(|h| h.to_string()).collect(), body.to_string())
+    }
+
     #[test]
     fn handle_http_request_valid_route() {
-        let request = vec!["GET / HTTP/1.1".to_string()];
-        let response = handle_http_request(&request).unwrap();
+        let (request, headers, body) = get_inputs("GET", "/", None, None);
+        let response = handle_http_request(&request, &headers, &body).unwrap();
         assert_eq!(response, b"HTTP/1.1 200 OK\r\n\r\n");
     }
 
     #[test]
     fn handle_http_request_invalid_route() {
-        let request = vec!["GET /foo HTTP/1.1".to_string()];
-        let response = handle_http_request(&request).unwrap();
+        let (request, headers, body) = get_inputs("GET", "/foo", None, None);
+        let response = handle_http_request(&request, &headers, &body).unwrap();
         assert_eq!(response, b"HTTP/1.1 404 Not Found\r\n\r\n");
     }
 
     #[test]
     fn handle_http_request_echo_route() {
-        let request = vec!["GET /echo/foo HTTP/1.1".to_string()];
-        let response = handle_http_request(&request).unwrap();
+        let (request, headers, body) = get_inputs("GET", "/echo/foo", None, None);
+        let response = handle_http_request(&request, &headers, &body).unwrap();
         assert_eq!(response, b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nfoo");
     }
 
     #[test]
     fn handle_http_request_user_agent_route() {
-        let request = vec!["GET /user-agent HTTP/1.1".to_string(), "User-Agent: curl/7.64.1".to_string()];
-        let response = handle_http_request(&request).unwrap();
+        let (request, headers, body) = get_inputs(
+            "GET", "/user-agent",
+            Some(vec!["User-Agent: curl/7.64.1"]),
+            None);
+        let response = handle_http_request(&request, &headers, &body).unwrap();
         assert_eq!(response, b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\ncurl/7.64.1");
     }
 
     #[test]
-    fn handle_http_request_file_route_valid() {
+    fn handle_http_request_get_file_route_valid() {
         env::set_var("APP_DIRECTORY", utils::get_project_source().unwrap_or_else(|| ".".to_string()));
-        let request = vec!["GET /files/test.txt HTTP/1.1".to_string()];
-        let response = handle_http_request(&request).unwrap();
+        let (request, headers, body) = get_inputs("GET", "/files/test.txt", None, None);
+        let response = handle_http_request(&request, &headers, &body).unwrap();
         assert_eq!(
             response,
             b"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: 13\r\n\r\nHello, World!"
@@ -126,23 +160,37 @@ mod tests {
     }
 
     #[test]
-    fn handle_http_request_file_route_invalid() {
-        let request = vec!["GET /files/random.txt HTTP/1.1".to_string()];
-        let response = handle_http_request(&request).unwrap();
+    fn handle_http_request_get_file_route_invalid() {
+        let (request, headers, body) = get_inputs("GET", "/files/random.txt", None, None);
+        let response = handle_http_request(&request, &headers, &body).unwrap();
         assert_eq!(response, b"HTTP/1.1 404 Not Found\r\n\r\n");
     }
 
     #[test]
-    fn handle_http_request_empty_request() {
-        let request = vec![];
-        let response = handle_http_request(&request);
-        assert!(response.is_err());
+    fn handle_http_request_post_file_route() {
+        env::set_var("APP_DIRECTORY", utils::get_project_source().unwrap_or_else(|| ".".to_string()));
+        let (request, headers, body) = get_inputs(
+            "POST",
+            "/files/abc.txt",
+            vec!["Content-Length: 5"].into(),
+            "abcde".into());
+        let response = handle_http_request(&request, &headers, &body).unwrap();
+        assert_eq!(response, b"HTTP/1.1 201 Created\r\n\r\n");
+
+        // Verify the file was created
+        let file_path = Path::new(&utils::get_project_source().unwrap_or_else(|| ".".to_string())).join("abc.txt");
+        let content = fs::read_to_string(&file_path).unwrap();
+        assert_eq!(content, "abcde");
+
+        // Cleanup
+        fs::remove_file(&file_path).expect("Cleanup failed");
     }
 
     #[test]
     fn handle_http_request_invalid_request() {
-        let request = vec!["GET".to_string()];
-        let response = handle_http_request(&request);
+        let (_request, headers, body) = get_inputs("GET", "/", None, None);
+        let request: String = String::from("GET");
+        let response = handle_http_request(&request, &headers, &body);
         assert!(response.is_err());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{Write, BufReader, BufRead, Error};
+use std::io::{Write, BufReader, BufRead, Error, Read};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::env;
@@ -36,22 +36,58 @@ fn main() {
     }
 }
 
+
+fn reader(buf_reader: &mut BufReader<&TcpStream>) -> (String, Vec<String>, String) {
+    let mut index: i32 = 0;
+    let mut request_line = String::new();
+    let mut headers = Vec::new();
+    let mut request_body = String::new();
+
+    let mut content_length: Option<usize> = None;
+
+    for line in buf_reader.by_ref().lines() {
+        let line = line.unwrap();
+        if index == 0 {
+            request_line = line;
+        } else {
+            if line.is_empty() {
+                break;
+            }
+            // Check for Content-Length header
+            if let Some(length) = line.strip_prefix("Content-Length: ") {
+                content_length = length.parse().ok();
+            }
+            headers.push(line);
+        }
+
+        index += 1;
+    }
+
+    // Read the request body if Content-Length header is present
+    if let Some(length) = content_length {
+        let mut body = vec![0; length];
+        buf_reader.read_exact(&mut body).unwrap();
+        request_body = String::from_utf8(body).unwrap();
+    }
+
+    println!("received request: {:?}, headers: {:?}, request body: {:?}", request_line, headers, request_body);
+
+    (request_line, headers, request_body)
+
+}
+
+
+
 fn process_request(stream: Result<TcpStream, Error>) {
 
     match stream {
         Ok(mut stream) => {
             println!("accepted new connection");
 
-            let buf_reader = BufReader::new(&mut stream);
+            let mut buf_reader = BufReader::new(&stream);
+            let (request_line, headers, request_body) = reader(&mut buf_reader);
 
-            // Vector is Rust dynamic growable array. _ means Rust will infer the type.
-            let http_request: Vec<_> = buf_reader
-                .lines()                                                // Get an iterator over lines. Returns Result<String, std::io::Error>
-                .map(|result| result.unwrap_or_else(|_| String::new())) // Unwrap the result or return an empty string
-                .take_while(|line| !line.is_empty())                    // HTTP Request ends with an empty line for version 1.1, so return as soon as you get it
-                .collect();                                             // Collect the iterator into a vector. This is just for easier processing - we can iterate over the lines directly too.
-
-            let response = handler::handle_http_request(&http_request)
+            let response = handler::handle_http_request(&request_line, &headers, &request_body)
                 .unwrap_or_else(|e| {
                     println!("Error processing request: {:?}", e);
                     b"HTTP/1.1 500 Internal Server Error\r\n\r\n".to_vec()
